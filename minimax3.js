d2ecc642d3ea377e7ac9fd4d4381eb405980aac4
Rishob Guha Patra <rguhapatra@gmail.com>
2020-04-15 20:14:56 -0400

    Ply at 3

--- minimax3.js
+++ minimax3.js
@@ -1,5 +1,7 @@
 var random = Math.random(-5,5);
 
+var count = 0;
+
 function bestMove() {
     // AI to make its turn
     let bestScore = -Infinity;
@@ -39,7 +41,7 @@ function bestMove() {
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
           // Is the spot available?
-          if (board[i][j] == '') {
+          if (board[i][j] == '' && count >= 3) {
             board[i][j] = ai;
             let score = minimax(board, depth + 1, false, alpha, beta);
             board[i][j] = '';
@@ -57,7 +59,7 @@ function bestMove() {
       for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
           // Is the spot available?
-          if (board[i][j] == '') {
+          if (board[i][j] == '' && count >= 3) {
             board[i][j] = human;
             let score = minimax(board, depth + 1, true, alpha, beta);
             board[i][j] = '';
